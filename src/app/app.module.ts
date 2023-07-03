12bc180f9f754546bc629a6f6afa5d408d46396d
almalmalm <almostkalinin@gmail.com>
2023-07-03 11:04:40 +0400

    fix: change imports for search results component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -8,13 +7,20 @@ import { SearchComponent } from './components/search/search.component';
 import { SortingComponent } from './components/sorting/sorting.component';
 import { FiltersComponent } from './components/filters/filters.component';
 import { PersonalComponent } from './components/personal/personal.component';
-import { BoardComponent } from './components/board/board.component';
-import { CardComponent } from './components/card/card.component';
-import { SearchResultsComponent } from './search-results/search-results.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
 import { SearchItemsComponent } from './components/search-items/search-items.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SearchComponent, SortingComponent, FiltersComponent, PersonalComponent, BoardComponent, CardComponent, SearchResultsComponent, SearchItemsComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    SortingComponent,
+    FiltersComponent,
+    PersonalComponent,
+    SearchResultsComponent,
+    SearchItemsComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
