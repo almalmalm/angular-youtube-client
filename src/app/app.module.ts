5b3091cb3d1e1a2d6998488e873fc04daa4b9c4c
almalmalm <almostkalinin@gmail.com>
2023-07-03 10:21:16 +0400

    feat: add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
+import { SortingComponent } from './components/sorting/sorting.component';
+import { FiltersComponent } from './components/filters/filters.component';
+import { PersonalComponent } from './components/personal/personal.component';
+import { BoardComponent } from './components/board/board.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, SearchComponent, SortingComponent, FiltersComponent, PersonalComponent, BoardComponent, CardComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
